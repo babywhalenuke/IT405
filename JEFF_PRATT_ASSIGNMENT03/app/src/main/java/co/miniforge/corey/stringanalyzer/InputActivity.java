cd44ee3f30acd5a8918731000739ba2b731df750
JEFF <jpratt@aim-aerospace.com>
2017-10-22 10:42:29 -0700

    assignment 3 string analyzer

---
+++ JEFF_PRATT_ASSIGNMENT03/app/src/main/java/co/miniforge/corey/stringanalyzer/InputActivity.java
@@ -0,0 +1,57 @@
+package co.miniforge.corey.stringanalyzer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class InputActivity extends AppCompatActivity {
+    public static String intentTag = "inputData";
+
+    EditText input;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        locateViews();
+
+        bindFunctionality();
+    }
+
+    void locateViews(){
+        input = (EditText) findViewById(R.id.stringToAnalyzeEditText);
+        submit = (Button) findViewById(R.id.submitButton);
+    }
+
+    void bindFunctionality(){
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(inputHasContent(input.getText().toString())){
+                Intent intent = new Intent(getApplicationContext(), AnalyzerActivity.class);
+                intent.putExtra(intentTag, input.getText().toString());
+                startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Please Enter A Message",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    boolean inputHasContent(String message){
+        if(message.length() > 0 && !message.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
